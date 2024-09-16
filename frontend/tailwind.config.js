8c7f470631e50caee4bffb1c98510def423c1bca
Crescens <2e3crescens@gmail.com>
2024-09-16 18:55:19 +0800

    fixed tailwind errors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,6 @@ export default {
   content: [
     "./index.html", // Add this if your root HTML file is in the project root
     "./src/**/*.{js,ts,jsx,tsx}", // Includes all JS, JSX, TS, and TSX files inside src folder
-    "./pages/**/*.{js,jsx,ts,tsx}", // For files in pages folder
   ],
   theme: {
     extend: {},
