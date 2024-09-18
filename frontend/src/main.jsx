acb34922f4afed81c719e998e36532faf73e6282
Crescens <2e3crescens@gmail.com>
2024-09-18 13:48:01 +0800

    snackbar/notistack minor bug fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { SnackBarProvider } from "notistack";
+import { SnackbarProvider } from "notistack";
+
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     {/* beautiful alert dependency */}
-    <SnackBarProvider>
+    <SnackbarProvider>
       <App />
-    </SnackBarProvider>
+    </SnackbarProvider>
   </BrowserRouter>
 );
